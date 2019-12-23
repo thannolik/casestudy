e1eca1fad90b741f5c1dc95850db104fb28f1f0e
Thannoli <thannoli.kadhiroli@nestle.com>
2019-12-23 10:57:05 +0530

    Updates

---
+++ precache-manifest.3031ffee7fc7d21546eaf640f8ab686a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27177f79d3fd945cf4e0f47172773f7d",
+    "url": "/casestudy/index.html"
+  },
+  {
+    "revision": "7d3fb569199a895c4297",
+    "url": "/casestudy/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "066baafdefb11288673d",
+    "url": "/casestudy/static/css/main.f6105d81.chunk.css"
+  },
+  {
+    "revision": "7d3fb569199a895c4297",
+    "url": "/casestudy/static/js/2.e5bcb93f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/casestudy/static/js/2.e5bcb93f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "066baafdefb11288673d",
+    "url": "/casestudy/static/js/main.e1b9d649.chunk.js"
+  },
+  {
+    "revision": "1736f252da4ff9783a5d",
+    "url": "/casestudy/static/js/runtime-main.776a1a58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/casestudy/static/media/logo.5d5d9eef.svg"
+  }
+]);
